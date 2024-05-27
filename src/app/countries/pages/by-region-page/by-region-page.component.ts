5fd1b302ac3ffc5cb8fd1db24ff5684bedff4982
kathm <kathmontalvo123@gmail.com>
2024-05-27 14:30:22 -0500

    mejoras, guardar info en cache

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -1,32 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Country } from '../../interfaces/country';
 import { CountriesService } from '../../services/countries.service';
-
-type Region = 'Africa'|'Americas'|'Asia'|'Europe'|'Oceania';
+import { Region } from '../../interfaces/region.type';
 
 @Component({
   selector: 'app-by-region-page',
   templateUrl: './by-region-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class ByRegionPageComponent {
+export class ByRegionPageComponent implements OnInit {
   public countries: Country[] = [];
   public isLoading: boolean = false;
-  public regions: Region[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+  public regions: Region[] = [
+    'Africa',
+    'Americas',
+    'Asia',
+    'Europe',
+    'Oceania',
+  ];
   public selectedRegion?: Region;
 
-  constructor( private countriesService: CountriesService) {}
+  constructor(private countriesService: CountriesService) {}
 
-   searchByRegion (region:Region):void {
+  ngOnInit(): void {
+      this.countries = this.countriesService.cacheStore.byRegion.countries;
+      this.selectedRegion = this.countriesService.cacheStore.byRegion.region;
+  }
+
+  searchByRegion(region: Region): void {
     this.selectedRegion = region;
     this.isLoading = true;
 
-    this.countriesService.searchRegion(region)
-    .subscribe( countries => {
+    this.countriesService.searchRegion(region).subscribe((countries) => {
       this.countries = countries;
       this.isLoading = false;
-    })
-
-   }
+    });
+  }
 }
